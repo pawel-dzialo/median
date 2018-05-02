311df1244dc671183247409f2d655073d455e79c
Pawel Dzialo <pakadz@gmail.com>
2018-05-02 13:12:00 +0200

    .

---
+++ Median/FileList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Median
+{
+    class FileList
+    {
+        string[] filePaths;
+        List<string> extensions;
+        public FileList(string[] paths) {
+            this.filePaths = paths;
+            setExtensions();
+        }
+
+        private void setExtensions() {
+            for (int i = 0; i < this.filePaths.Length; i++)
+            {
+                for (int j = this.filePaths[i].Length; j >= 0; j--)
+                {
+                    string extension = "";
+                    if (filePaths[i][j] == '.')
+                    {
+                        if (extensions.Count() == 0)
+                        {
+                            extensions.Add(extension);
+                            extension = "";
+                            break;
+                        }
+                        else
+                        {
+                            Boolean exists = false;
+                            for (int k = 0; k < extensions.Count; k++)
+                            {
+                                if (extension == extensions[k])
+                                {
+                                    exists = true;
+                                    break;
+                                }
+                            }
+                            if (!exists)
+                            {
+                                extensions.Add(extension);
+                                extension = "";
+                            }
+
+                        }
+                        
+                    }
+                    else
+                    {
+                        extension = filePaths[i][j].ToString() + extension;
+                    }
+                }
+            }
+        }
+        public List<string> getExtensions() {
+            return this.extensions;
+        }
+        public string[] getPaths() {
+            return this.filePaths;
+        }
+    }
+}
--- Median/Form1.cs
+++ Median/Form1.cs
@@ -12,7 +12,7 @@ namespace Median
 {
     public partial class Form1 : Form
     {
-        string[] filePaths;
+        FileList list;
         public Form1()
         {
             InitializeComponent();
@@ -25,38 +25,7 @@ namespace Median
         private void button1_Click(object sender, EventArgs e)
         {
             if (openMedia.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
-                filePaths = openMedia.FileNames;
-                List<string> extensions = new List<string>();
-                for (int i = 0; i < filePaths.Length; i++) {
-                    for (int j = filePaths[i].Length; j >= 0; j--) {
-                        string extension = "";
-                        if (filePaths[i][j] == '.')
-                        {
-                            if (extensions.Count() == 0)
-                            {
-                                extensions.Add(extension);
-                                break;
-                            }
-                            else {
-                                Boolean exists = false;
-                                for (int k = 0; k < extensions.Count; k++) {
-                                    if (extension == extensions[k]) {
-                                        exists = true;
-                                        break;
-                                    }
-                                }
-                                if (!exists) {
-                                    extensions.Add(extension);
-                                }
-
-                            }
-                            break;
-                        }
-                        else {
-                            extension = filePaths[i][j].ToString() + extension;
-                        }
-                    }
-                }
+                list = new FileList(openMedia.FileNames);
             }
         }
     }
